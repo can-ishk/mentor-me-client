a13afccf58e2b2b33be1671f3db41c5858363c7b
can-ishk <kchathley26@gmail.com>
2023-04-25 00:54:52 +0530

    prod final

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // const NODE_ENV = "DEV"
-const NODE_ENV = "DEV"
+const NODE_ENV = "PROD"
 const BASE_URL_DEV = "http://localhost:8000/"
 const BASE_URL = NODE_ENV==="DEV"?BASE_URL_DEV:"https://mentor-me-server.vercel.app/"
 
