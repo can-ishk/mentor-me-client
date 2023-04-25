4ae384db094817dce5a89ece766823d005cdf42d
can-ishk <kchathley26@gmail.com>
2023-04-25 15:58:42 +0530

    skills

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -16,10 +16,33 @@ import ErrorAlert from '../components/ErrorAlert';
 import Footer from '../components/Footer';
 import { signup } from "../handlers/users";
 import { logInUser } from "../helpers/authHelper";
-import options from '../data/options.js'
 
-
-
+const depts = [
+    { value: "Computer Science & Engineering", label: "Computer Science & Engineering" },
+  { value: "Chemical Engineering", label: "Chemical Engineering" },
+  { value: "Civil Engineering", label: "Civil Engineering" },
+  { value: "Electrical Engineering", label: "Electrical Engineering" },
+  { value: "Mechanical Engineering", label: "Mechanical Engineering" },
+  { value: "Art, Media and Performance", label: "Art, Media and Performance" },
+  { value: "Design", label: "Design" },
+  { value: "Economics", label: "Economics" },
+  { value: "English", label: "English" },
+  { value: "History and Archaeology", label: "History and Archaeology" },
+  { value: "International Relations and Governance Studies", label: "International Relations and Governance Studies" },
+  { value: "Sociology", label: "Sociology" },
+  { value: "Decision Sciences, Operations Management, and Information Systems", label: "Decision Sciences, Operations Management, and Information Systems" },
+  { value: "Economics and Public Policy", label: "Economics and Public Policy" },
+  { value: "Finance, Accounting and Control", label: "Finance, Accounting and Control" },
+  { value: "General Management", label: "General Management" },
+  { value: "Marketing Management", label: "Marketing Management" },
+  { value: "Organization Behavior and Human Resource Management", label: "Organization Behavior and Human Resource Management" },
+  { value: "Strategy and Entrepreneurship", label: "Strategy and Entrepreneurship" },
+  { value: "Chemistry", label: "Chemistry" },
+  { value: "Life Sciences", label: "Life Sciences" },
+  { value: "Mathematics", label: "Mathematics" },
+  { value: "Physics", label: "Physics" },
+  { value: "Biology", label: "Biology" },
+]
 export default function SignUp() {
     const navigate = useNavigate();
     const [serverError, setServerError] = useState("");
@@ -144,7 +167,7 @@ export default function SignUp() {
                         error={errors.dept !== undefined}
                         helperText={errors.dept}
                     >
-                        {options.map((option) => (
+                        {depts.map((option) => (
                             <MenuItem value={option.value}>{option.label}</MenuItem>
                         ))}
                     </TextField>
